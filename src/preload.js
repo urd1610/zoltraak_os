8369f83e3e3a9d9efa72a500bfc58b26273601c3
urd <que.sera.sera1610@gmail.com>
2025-12-02 13:02:35 +0900

    workspaceエントリを開くIPCを追加

--- src/preload.js
+++ src/preload.js
@@ -107,6 +107,7 @@ contextBridge.exposeInMainWorld('desktopBridge', {
   getWorkspaceDirectory: () => ipcRenderer.invoke('workspace:get-directory'),
   changeWorkspaceDirectory: () => ipcRenderer.invoke('workspace:change-directory'),
   openWorkspaceDirectory: () => ipcRenderer.invoke('workspace:open-directory'),
+  openWorkspaceEntry: (entryId) => ipcRenderer.invoke('workspace:open-entry', entryId),
   getWorkspaceGraph: () => ipcRenderer.invoke('workspace:get-graph'),
   saveRecording: (buffer, mimeType) => ipcRenderer.invoke('recording:save', { buffer, mimeType }),
   getAiMailStatus: () => ipcRenderer.invoke('ai-mail:get-status'),
